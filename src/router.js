d4792b7bff85ce952eca5ff61e0f28fdc995e4b1
rtfmtom <careythomas91@gmail.com>
2025-01-03 01:38:14 -0600

    refactored router, pulled out html templates

--- src/router.js
+++ src/router.js
@@ -1,19 +1,32 @@
-export default function router() {
+import homeTemplate from "./templates/home.js";
+import aboutTemplate from "./templates/about.js";
+import contactTemplate from "./templates/contact.js";
 
-  // define your routes
+export default function router() {
   const routes = {
     home: "/",
     about: "/about",
     contact: "/contact",
   };
 
+  const templates = {
+    [routes.home]: homeTemplate,
+    [routes.about]: aboutTemplate,
+    [routes.contact]: contactTemplate,
+  };
+
   return {
-    currentRoute: routes.home,
+    currentRoute: "",
+    html: "",
 
     init() {
       this.currentRoute = window.location.hash.slice(1) || routes.home;
+      this.html =
+        templates[this.currentRoute] || `<div><h1>404 Not Found</h1></div>`;
       window.addEventListener("hashchange", () => {
         this.currentRoute = window.location.hash.slice(1) || routes.home;
+        this.html =
+          templates[this.currentRoute] || `<div><h1>404 Not Found</h1></div>`;
       });
     },
 
@@ -21,44 +34,24 @@ export default function router() {
       window.location.hash = route;
     },
 
-    // bind click events to this.navigate(route)
-    link: {
-      home: {
-        href: routes.home,
-        ["@click.prevent"]() {
-          this.navigate(routes.home);
-        },
-      },
-      about: {
-        href: routes.about,
-        ["@click.prevent"]() {
-          this.navigate(routes.about);
-        },
-      },
-      contact: {
-        href: routes.contact,
-        ["@click.prevent"]() {
-          this.navigate(routes.contact);
-        },
-      },
-    },
-
-    // conditionally display routes using x-if
     home: {
-      ["x-if"]() {
-        return this.currentRoute === routes.home;
+      href: routes.home,
+      ["@click.prevent"]() {
+        this.navigate(routes.home);
       },
     },
 
     about: {
-      ["x-if"]() {
-        return this.currentRoute === routes.about;
+      href: routes.about,
+      ["@click.prevent"]() {
+        this.navigate(routes.about);
       },
     },
 
     contact: {
-      ["x-if"]() {
-        return this.currentRoute === routes.contact;
+      href: routes.contact,
+      ["@click.prevent"]() {
+        this.navigate(routes.contact);
       },
     },
   };
---
+++ src/templates/home.js
@@ -0,0 +1,6 @@
+export default /*html*/ `
+<div>
+  <h1>Home Page</h1>
+  <p>Welcome to the home page!</p>
+</div>
+`;
